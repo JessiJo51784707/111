840cb7971e538681e881ed62d814d7b4875b4ad9
JessiJo51784707 <102207162+JessiJo51784707@users.noreply.github.com>
2022-03-23 08:37:00 +0300

    Create spam.js

---
+++ spam.js
@@ -0,0 +1,11 @@
+<p>E-Mail: <b><a id='mail1' href=''></a></b></p>
+
+<script>
+onDomReady(function(){
+  var mail="kdg"+'@'; mail=mail+'htmlweb.ru';
+  if(el=document.getElementById('mail1')){
+	el.appendChild( document.createTextNode( mail ) );
+	el.href="mai"+"lto:"+mail;
+	}
+});
+</script>
